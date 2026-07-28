afcb11caa420d4fc34054b12c848a93f687fd553
Gorgovan Tudor <gorgovantudor@gmail.com>
2026-07-28 22:13:08 +0300

    Update SQL query to group by category and enhance bar chart visualization

--- src/PAS_2_Rapoarte/2_flexibilitate.py
+++ src/PAS_2_Rapoarte/2_flexibilitate.py
@@ -7,13 +7,17 @@ conn = sqlite3.connect('./data/case/case_profil_orar.sqlite3')
 cursor = conn.cursor()
 query = """
 SELECT casa,
+    categorie,
     COUNT(DISTINCT id_appliance) AS nr_aparate
 FROM profil_orar
 WHERE controlabil = 1
-GROUP BY casa
+GROUP BY casa, categorie
 
 """
 df_apl_ctrb = pd.read_sql_query(query, conn)
 #print(df_apl_ctrb)
-
-px.bar(df_apl_ctrb.sort_values('nr_aparate', ascending=False), x='casa', y='nr_aparate',color = 'nr_aparate').show()
+ordine = (df_apl_ctrb.groupby('casa')['nr_aparate']
+          .sum()
+          .sort_values(ascending=False)
+          .index.tolist())
+px.bar(df_apl_ctrb, x='casa', y='nr_aparate',color = 'categorie',category_orders={'casa': ordine}).show()
